64cf9d99404dfcef7a8a75f7be068c5f978d0ccd
Alexey <ogobel@icloud.com>
2021-02-19 20:20:29 +0300

    feat: add rowelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr1 = [];
+    if(matrix){
+     let k=[];
+    for(let i = 0; i<matrix.length;i++){
+        k =  matrix[i];
+        if(i%2==0){
+        for(let j = 0; j<k.length; j++){
+            arr1.push(parseInt(matrix[i][j]));
+        }
+      }
+        else{
+          for(let j = k.length-1; j>=0; j--){
+            arr1.push(parseInt(matrix[i][j]));
+        }
+    }
+  }
 }
+  return   arr1;
+  }
